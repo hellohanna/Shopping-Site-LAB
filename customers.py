dbfe120e4dee1a05fbb3cfac213ebb4a3cae0485
Hanna Babushkina <anylike90@gmail.com>
2018-09-20 05:49:41 +0000

    Add customers.py

--- customers.py
+++ customers.py
@@ -4,4 +4,27 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self,first_name,last_name,email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        """Convenience method to show information"""
+
+        return "<Customer: {}, {}, {}". format(self.first_name, 
+                                                self.last_name, 
+                                                self.email)
+
+    customers = {}
+
+    with open("customers.txt") as file:
+        for line in file:
+            (first_name, last_name, email,password) = line.strip().split("|")
+            customers[email] = Customer(first_name, last_name, email, password)
+
+    def get_by_email(email):
+        """Return a customer, given its email"""
+
+        return customers[email]
